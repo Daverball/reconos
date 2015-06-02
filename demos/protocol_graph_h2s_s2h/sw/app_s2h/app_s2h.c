3e3010531f2a11ff99b193fc77787d7f412b7ced
David Salvisberg <dave@portablegaming.de>
2015-06-02 16:18:24 +0200

    Updated app_s2h to work with multiple packets per buffer.

--- demos/protocol_graph_h2s_s2h/sw/app_s2h/app_s2h.c
+++ demos/protocol_graph_h2s_s2h/sw/app_s2h/app_s2h.c
@@ -28,8 +28,9 @@
 #define TO_ETH 4
 #define TO_FB1 1
 
-#define PACKET_LENGTH_1 64
-#define PACKET_LENGTH_2 84
+#define DEFAULT_BUFFER_SIZE 2048
+
+#define DEFAULT_PACKET_SIZE 64
 
 struct reconos_resource res[NUM_SLOTS][2];
 struct reconos_hwt hwt[NUM_SLOTS];
@@ -38,6 +39,8 @@ struct mbox mb_out[NUM_SLOTS];
 
 unsigned char *shared_mem_h2s;
 unsigned char *shared_mem_s2h;
+
+unsigned int buffer_size = DEFAULT_BUFFER_SIZE;
 
 struct noc_header {
 	unsigned char hw_addr_switch:4,
@@ -68,10 +71,10 @@ void config_eth(unsigned int hash_1, unsigned int hash_2, unsigned int idp, int 
 
 void setup_noc(void)
 {
-	shared_mem_h2s = malloc(4096); 
-	memset(shared_mem_h2s, 0, 4096);
-	shared_mem_s2h = malloc(4096); 
-	memset(shared_mem_s2h, 0, 4096);
+	shared_mem_h2s = malloc(buffer_size); 
+	memset(shared_mem_h2s, 0, buffer_size);
+	shared_mem_s2h = malloc(buffer_size); 
+	memset(shared_mem_s2h, 0, buffer_size);
 	mbox_put(&mb_in[HWT_H2S], (unsigned int) shared_mem_h2s);
 	mbox_put(&mb_in[HWT_S2H], (unsigned int) shared_mem_s2h);
 	config_eth(0xabababab, 0xabababab, 3, TO_H2S); // SW
@@ -84,24 +87,43 @@ void setup_noc(void)
 // MAIN ////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[])
 {
-	int i, msg_len,cnt=0;
+	int i, msg_len, cnt=0, max_cnt=5;
 	unsigned char * ptr;
-	unsigned char * ptr2;
 	unsigned int * ptr_int;
-	unsigned int * ptr2_int;
-	unsigned int packet_select = 0;
+	unsigned int packet_size = DEFAULT_PACKET_SIZE;
+	unsigned int aligned_size;
+	unsigned int num_packets;
 
 	printf( "-------------------------------------------------------\n"
 		    "APP DEMO\n"
 		    "(" __FILE__ ")\n"
 		    "Compiled on " __DATE__ ", " __TIME__ ".\n"
 		    "-------------------------------------------------------\n\n" );
+	if(argc > 1 && atoi(argv[1])>0 && atoi(argv[1])<65 )
+	{
+		buffer_size *= atoi(argv[1]);
+	} else {
+		printf("[app] Received no valid buffer size in KB for argument 1 using default.\n");
+	}
+	
+	if(argc > 2 && atoi(argv[2])>63 && atoi(argv[2])<1501 )
+	{
+		packet_size = atoi(argv[2]);
+	} else {
+		printf("[app] Received no valid packet size for argument 2 using default.\n");
+	}
+	aligned_size = 4*((packet_size+3)/4); //round up to next word boundary
+	assert((aligned_size & 3) == 0); // make sure size is aligned
+	num_packets = (buffer_size-1500-4)/aligned_size + 1; // make this a bit more robust maybe, good estimate for now though
+	
+	printf("[app] Sending packets of size %d with a buffer of size %d with %d packets per buffer.\n", packet_size, buffer_size, num_packets);
 
 	printf("[app] Initialize ReconOS.\n");
 	reconos_init_autodetect();
 
 	printf("[app] Creating delegate threads.\n\n");
-	for (i=0; i<NUM_SLOTS; i++){
+	for (i=0; i<NUM_SLOTS; i++)
+	{
 		// mbox init
 		mbox_init(&mb_in[i],  10);
 		mbox_init(&mb_out[i], 10);
@@ -119,103 +141,68 @@ int main(int argc, char *argv[])
 	setup_noc();
 
 	ptr = (unsigned char *) shared_mem_s2h;
-	ptr2 = (unsigned char *) (shared_mem_s2h+2048);
 	ptr_int = (unsigned int *) ptr;
-	ptr2_int = (unsigned int *) ptr2;
 	printf("[app] Sending packets...\n");
 
-	// generate packet packet 1 - NoC header
-	struct noc_header * noc_header_1 = malloc(sizeof(struct noc_header));
-	noc_header_1->hw_addr_switch = 1;
-	noc_header_1->hw_addr_block = 0;
-	noc_header_1->priority = 0;
-	noc_header_1->direction = 0;
-	noc_header_1->latency_critical = 0;
-	noc_header_1->reserved = 0;
-	noc_header_1->payload_len = PACKET_LENGTH_1;
-	noc_header_1->src_idp = 3;
-	noc_header_1->dst_idp = 2;
-	memcpy(ptr,noc_header_1,sizeof(struct noc_header));
+	// generate packet
+	unsigned char * packet = malloc(packet_size);
+	struct noc_header * noc_header = (struct noc_header *) packet;
+	noc_header->hw_addr_switch = 1;
+	noc_header->hw_addr_block = 0;
+	noc_header->priority = 0;
+	noc_header->direction = 0;
+	noc_header->latency_critical = 0;
+	noc_header->reserved = 0;
+	noc_header->payload_len = packet_size-12;
+	noc_header->src_idp = 3;
+	noc_header->dst_idp = 2;
 
-	// set packet data 1
-	for(i=0;i<PACKET_LENGTH_1;i++)
+	// set packet payload, start at byte 12
+	for(i=12;i<packet_size;i++)
 	{
-		ptr[i+sizeof(struct noc_header)] = (unsigned char) (i+1);
-	}
-
-	printf("\n\n[app] print packet 1\n\t");
-	for (i=0;i<((PACKET_LENGTH_1+sizeof(struct noc_header))/4);i++)
-	{
-		printf("0x%08x  ",ptr_int[i]);
-		if ((i+1)%4==0)
-		{ 
-			printf("\n\t");
-		}
-	}
-
-	// generate packet packet 2 - NoC header
-	struct noc_header * noc_header_2 = malloc(sizeof(struct noc_header));
-	noc_header_2->hw_addr_switch = 1;
-	noc_header_2->hw_addr_block = 0;
-	noc_header_2->priority = 0;
-	noc_header_2->direction = 0;
-	noc_header_2->latency_critical = 0;
-	noc_header_2->reserved = 0;
-	noc_header_2->payload_len = PACKET_LENGTH_2;
-	noc_header_2->src_idp = 2;
-	noc_header_2->dst_idp = 3;
-	memcpy(ptr2,noc_header_2,sizeof(struct noc_header));
-
-	// set packet data 2
-	for(i=0;i<PACKET_LENGTH_2;i++)
-	{
-		ptr2[i+sizeof(struct noc_header)] = (unsigned char) (i+20);
-	}
-
-	printf("\n\n[app] print packet 2\n\t");
-	for (i=0;i<((PACKET_LENGTH_2+sizeof(struct noc_header))/4);i++)
-	{
-		printf("0x%08x  ",ptr2_int[i]);
-		if ((i+1)%4==0)
-		{ 
-			printf("\n\t");
-		}
+		packet[i] = (unsigned char) (i-11);
 	}
 
 	printf("\n\n");
+	
+	// copy packets into buffer
+	for(i=0;i<num_packets;i++)
+	{
+		memcpy(ptr+4+i*aligned_size, packet, packet_size);
+	}
+
+	printf("\n\n[app] print buffer");
+	*ptr_int = num_packets;
+	for (i=0;i<((buffer_size)/4);i++)
+	{
+		if (i%4==0)
+		{ 
+			printf("\n\t");
+		}
+		printf("0x%08x  ",ptr_int[i]);
+	}
+	printf("\n");
 
 	// flush cache
 	reconos_cache_flush();
 
 	// send packets
-	while(1) 
+	while(cnt < max_cnt) 
 	{
 		// flush cache
 		//reconos_cache_flush();
 
 		// 1. send length
-		if (packet_select==0)
-			mbox_put(&mb_in[HWT_S2H], (unsigned int) (PACKET_LENGTH_1+12)); // NoC header (12 bytes) + packet length 1
-		else
-			mbox_put(&mb_in[HWT_S2H], (unsigned int) (PACKET_LENGTH_2+12)); // NoC header (12 bytes) + packet length 2
+		mbox_put(&mb_in[HWT_S2H], (unsigned int) aligned_size*num_packets);
 
 		// 2. wait for ack
 		msg_len = mbox_get(&mb_out[HWT_S2H]);
 
 		// 3. set next s2h shared mem addr
-		if (packet_select==0)
-		{
-			packet_select=1;
-			mbox_put(&mb_in[HWT_S2H], (unsigned int) ptr2);
-		}
-		else
-		{
-			packet_select=0;
-			mbox_put(&mb_in[HWT_S2H], (unsigned int) ptr);
-		}
+		mbox_put(&mb_in[HWT_S2H], (unsigned int) ptr);
 
 		cnt++;
-		printf("sent packet no. %05d\n", cnt);
+		printf("sent buffer no. %05d\n", cnt);
 
 	}
 	printf("\n");
